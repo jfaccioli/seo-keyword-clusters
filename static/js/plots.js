c52367539535c05e63b18850b37b884dce5dd106
Jimmy Faccioli <77733975+jfaccioli@users.noreply.github.com>
2021-08-26 15:17:35 +0800

    final push

--- static/js/plots.js
+++ static/js/plots.js
@@ -8,40 +8,7 @@ d3.json(keyword_clustering_data).then((importedData) => {
     var impressions = importedData.Impressions;
     var position = importedData.Position;
     var top_queries = importedData["Top queries"];
-    console.log(importedData);
-
-    
-    // var myArray = [
-    //     {'name':Object.values(click_rate)[0], 'age':clicks, 'birthdate':impressions},
-    //     {'name':'Mila', 'age':'32', 'birthdate':'10/1/1989'},
-    //     {'name':'Paul', 'age':'29', 'birthdate':'10/14/1990'},
-    //     {'name':'Dennis', 'age':'25', 'birthdate':'11/29/1993'},
-    //     {'name':'Tim', 'age':'27', 'birthdate':'3/12/1991'},
-    //     {'name':'Erik', 'age':'24', 'birthdate':'10/31/1995'},
-    // ]
-    
-    // buildTable(myArray)
-
-
-
-    // function buildTable(data){
-    //     var table = document.getElementById('myTable')
-
-    //     for (var i = 0; i < data.length; i++){
-    //         var row = `<tr>
-    //                         <td>${data[i].name}</td>
-    //                         <td>${data[i].age}</td>
-    //                         <td>${data[i].birthdate}</td>
-    //                   </tr>`
-    //         table.innerHTML += row
-
-
-    //     }
-    // }
-
-
-
-    
+    console.log(importedData);    
 
     
 });
